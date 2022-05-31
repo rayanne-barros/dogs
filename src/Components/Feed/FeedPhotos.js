b890e6c32f680e857e752eb01da8d90c2f7bfb8a
rayanne-barros <rayanneb126@gmail.com>
2022-05-30 21:54:24 -0300

    Modal pronto

--- src/Components/Feed/FeedPhotos.js
+++ src/Components/Feed/FeedPhotos.js
@@ -6,7 +6,7 @@ import { PHOTOS_GET } from '../../api';
 import Loading from '../Help/Loading';
 import styles from './FeedPhotos.module.css';
 
-const FeedPhotos = () => {
+const FeedPhotos = ({setModalPhoto}) => {
 
   const { data, loading, error, request } = useFetch();
 
@@ -24,7 +24,8 @@ const FeedPhotos = () => {
   if (data)
     return (
       <ul className={`${styles.feed} animeLeft`}>
-        {data.map((photo) => <FeedPhotosItem key={photo.id} photo={photo} />)}
+        {data.map((photo) => (
+        <FeedPhotosItem key={photo.id} photo={photo} setModalPhoto={setModalPhoto} />))}
       </ul>
     );
   else return null
